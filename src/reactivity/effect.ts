0a5bc38556b5a9e2fe2504a58f8e6f9626e02bc8
姬顶盒 <setoboxes@gmail.com>
2023-04-25 08:51:39 +0800

    feat: effect track & trigger

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -5,11 +5,37 @@ class ReactiveEffect {
     this._fn = fn
   }
   run() {
+    activeEffect = this
     this._fn()
   }
 
 }
 
+let targetMap = new Map()
+export function track(target, key) {
+  let depsMap = targetMap.get(target)
+  if (!depsMap) {
+    depsMap = new Map()
+    targetMap.set(target, depsMap)
+  }
+  let dep = depsMap.get(key)
+  if (!dep) {
+    dep = new Set()
+    depsMap.set(key, dep)
+  }
+  dep.add(activeEffect)
+}
+
+export function trigger(target, key) {
+  let depsMap = targetMap.get(target)
+  let dep = depsMap.get(key)
+
+  for (const effect of dep) {
+    effect.run()
+  }
+}
+
+let activeEffect
 export function effect(fn) {
   const _effect = new ReactiveEffect(fn)
 
